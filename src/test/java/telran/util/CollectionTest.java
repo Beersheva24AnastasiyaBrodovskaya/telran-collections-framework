7dfec456930595d1d0b5f0bb80763a3ea24c7b23
Anastasiya Brodovskaya <leviyash@gmail.com>
2024-09-03 14:03:47 +0300

    HW_24_1

--- src/test/java/telran/util/CollectionTest.java
+++ src/test/java/telran/util/CollectionTest.java
@@ -2,12 +2,14 @@ package telran.util;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
